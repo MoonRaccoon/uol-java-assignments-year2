007021e55096025cc00c57d5a03623b7ad096611
Shamoun Syed <shamoun723@gmail.com>
2017-12-22 13:35:09 -0800

    complete part A

--- CirclesGUI.java
+++ CirclesGUI.java
@@ -1,14 +1,20 @@
+/*
+ * Name: Shamoun Syed
+ * Student Number: 140313701
+*/
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 
-public class CirclesGUI implements MouseListener{
+public class CirclesGUI {
 	JFrame frame;
 	CircleDrawPanel drawPanel;
 	private Color color = Color.red;
 	private int X; private int Y;
 	private int diameter = 100;
+    private Random generator;
 
 
 	public static void main (String[] args){
@@ -20,26 +26,25 @@ public class CirclesGUI implements MouseListener{
 	public void go(){
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        generator = new Random();
 
 		drawPanel = new  CircleDrawPanel();
 		frame.getContentPane().add(BorderLayout.CENTER, drawPanel);
-		drawPanel.addMouseListener(this);
+		drawPanel.addMouseListener(new CircleListener());
 
+        JButton resizeButton = new JButton("click to resize the circle");
+        frame.getContentPane().add(BorderLayout.NORTH, resizeButton);
+        resizeButton.addActionListener(new DiameterListener());
+
+        JButton colorButton = new JButton("click to change the color of the circle");
+        frame.getContentPane().add(BorderLayout.SOUTH, colorButton);
+        colorButton.addActionListener(new RandomColourListener());
+
+        X = 300; // Set initial position to be near the center of the screen,
+        Y = 300; // for immediate visibility upon running the program
 		frame.setSize(600,600);
 		frame.setVisible(true);
 	}
-
-	    public void mousePressed(MouseEvent e) {}
-	    public void mouseReleased(MouseEvent e) {}
-	    public void mouseEntered(MouseEvent e) {}
-	    public void mouseExited(MouseEvent e) {}
-	    //all interface methods must be implemented
-
-	    public void mouseClicked(MouseEvent e) {
-	      X = e.getX();
-	      Y = e.getY();
-	      drawPanel.repaint();
-		}
 
 
 	//the placing of the circle depends on the X and Y instance variables. CircleDrawPanel can access the X and Y variables, as inner classes have unrestricted access to their containing classes instance variables.
@@ -51,4 +56,37 @@ public class CirclesGUI implements MouseListener{
 			g2.fillOval(X - diameter/2, Y - diameter/2, diameter, diameter);
 		}
 	}
+
+    class CircleListener implements MouseListener {
+        public void mousePressed(MouseEvent e) {}
+	    public void mouseReleased(MouseEvent e) {}
+	    public void mouseEntered(MouseEvent e) {}
+	    public void mouseExited(MouseEvent e) {}
+	    //all interface methods must be implemented
+
+	    public void mouseClicked(MouseEvent e) {
+            X = e.getX();
+            Y = e.getY();
+            drawPanel.repaint();
+		}
+    }
+
+    class DiameterListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            diameter = generator.nextInt(600);
+            drawPanel.repaint();
+        }
+    }
+
+    class RandomColourListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            Color c = new Color(
+                    generator.nextInt(255),
+                    generator.nextInt(255),
+                    generator.nextInt(255));
+
+            color = c;
+            drawPanel.repaint();
+        }
+    }
 }
